95ba4f17a460d9012eb4731525561fc5b70babc1
lill <lill@knoyism.net>
2023-09-08 19:09:53 +0900

    first commit.

---
+++ src/commands.js
@@ -0,0 +1,19 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const AWW_COMMAND = {
+  name: 'awwww',
+  description: 'Drop some cuteness on this channel.',
+};
+
+export const INVITE_COMMAND = {
+  name: 'invite',
+  description: 'Get an invite link to add the bot to your server',
+};
+
+export const GEN_NIGHTMARKET = {
+  name: 'nightmarket',
+  description: 'ナイトマーケットを生成します',
+};
